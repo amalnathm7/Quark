c4294961fc0c58652d0932e80381b8f80b06d061
Meenakshi Nair <meenakshin@firstbytedigital.com>
2021-12-04 19:55:04 +0530

    Electric car

---
+++ lib/main_screen/electric_car_page/electric_car.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ElectricCar extends StatefulWidget {
+  @override
+  _ElectricCarState createState() => _ElectricCarState();
+}
+
+class _ElectricCarState extends State<ElectricCar> {
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        height: screenHeight,
+        width: screenWidth,
+        color: Colors.white,
+        child: const Text(
+            'Hi',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20),
+        ),
+      )
+    );
+  }
+}
