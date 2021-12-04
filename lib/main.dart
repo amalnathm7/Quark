4e34b77488de8b2260cb106a61cafc367796171a
Dinoy-Raj <dinoykraj.com>
2021-12-04 11:11:41 +0530

    added login page

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -33,15 +34,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,18 +41,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      body: Container(
+      body: SizedBox(
         height: screenHeight,
         width: screenWidth,
         child: Column(
@@ -83,9 +64,12 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             ElevatedButton(onPressed: (){
               //
-              // Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()));
-            }, child: Text("login"),),
-            ElevatedButton(onPressed: (){}, child: Text("home"),),
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>const LoginScreen()));
+            }, child: const Text("login"),),
+            ElevatedButton(onPressed: (){
+
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>const LoginScreen()));
+            }, child: const Text("home"),),
           ],
         ),
       ),// This trailing comma makes auto-formatting nicer for build methods.
