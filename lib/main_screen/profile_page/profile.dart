e640d411d196cc9e2b510fd20fe048837fe202d0
Amal Nath M <amalnathm7@gmail.com>
2021-12-04 23:57:32 +0530

    Profile

--- lib/main_screen/profile_page/profile.dart
+++ lib/main_screen/profile_page/profile.dart
@@ -1,15 +1,180 @@
+import 'dart:io';
+import 'dart:ui';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quark/firebase/authentication/auth.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
-  _ProfilePageState createState() => _ProfilePageState();
+  ProfilePageState createState() => ProfilePageState();
 }
 
-class _ProfilePageState extends State<ProfilePage> {
+class ProfilePageState extends State<ProfilePage>
+    with SingleTickerProviderStateMixin {
+  bool _isLogOutClicked = false;
+  static bool isLoading = false;
+  final _user = FirebaseAuth.instance.currentUser!;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: const Color(0xFFf6fafd),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Stack(
+                    children: [
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            top: 10,
+                            bottom: 0,
+                          ),
+                          child: SizedBox(
+                            height: 250,
+                            width: screenWidth,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                "https://images.pexels.com/photos/3031397/pexels-photo-3031397.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Text(
+                          _user.displayName!,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
+                          ),
+                        ),
+                        Text(
+                          _user.email!,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text(
+                          "Household",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 80,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: Image.asset('assets/logo.png'),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: const [
+                  Text(
+                    "Save Energy\nFor All Life\n\n💡",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "- A Hack4TKM Initiative by Team Gryffindor",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 10,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            _isLogOutClicked
+                ? const CupertinoActivityIndicator()
+                : Padding(
+                    padding: const EdgeInsets.only(
+                        left: 15, right: 15, top: 30, bottom: 30),
+                    child: OutlinedButton(
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        side: MaterialStateProperty.all(
+                          const BorderSide(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      onPressed: () async {
+                        setState(() {
+                          _isLogOutClicked = true;
+                        });
+                        await Authentication().signOut();
+                        setState(() {
+                          _isLogOutClicked = false;
+                        });
+                      },
+                      child: TextButton.icon(
+                        icon: Icon(
+                          Icons.logout_sharp,
+                          color: Colors.grey[700],
+                        ),
+                        label: Text(
+                          "Log out",
+                          style: TextStyle(
+                              color: Colors.grey[700],
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        onPressed: null,
+                      ),
+                    ),
+                  ),
+          ],
+        ),
+      ),
+    );
   }
 }
